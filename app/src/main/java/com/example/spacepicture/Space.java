9b95852e4c5a7512afd40370ab3543ec761473ed
AndrewAntepa <55337950+AndrewAntepa@users.noreply.github.com>
2021-04-13 11:11:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/spacepicture/Space.java
@@ -0,0 +1,11 @@
+package com.example.spacepicture;
+
+public class Space {
+    public String copyright = "";
+    public String date = "";
+    public String explanation = "";
+    public String hdurl = "";
+    public String media_type = "";
+    public String service_version = "";
+    public String url = "";
+}
